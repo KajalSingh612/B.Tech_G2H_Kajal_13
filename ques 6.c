6b05b64cbdb81bf458e72743b53ddc32b03dc6a5
KajalSingh612 <25110cn607@glbitm.ac.in>
2026-02-19 22:53:31 -0800

    Add files via upload

---
+++ ques 6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    float a,b;
+    printf("enter two numbers \n");
+    scanf("%f %f" ,&a,&b);
+    if (a==b)
+    { 
+    printf("two numbers are equal");
+    }
+    else
+    {
+    printf("numbers are not equal");
+    }
+return 0;
+}
