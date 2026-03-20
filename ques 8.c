1e7e99fe71d026d7cf96b63075415b444d4bdb2f
KajalSingh612 <25110cn607@glbitm.ac.in>
2026-03-20 07:09:15 +0530

    Create ques 8.c

---
+++ ques 8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{ int a;
+printf("Enter no. to check \n");
+scanf("%d", &a);
+if(a%2==0)
+{ printf("even no.");
+}
+else
+{ printf("odd no.");
+}
+return 0;
+}
